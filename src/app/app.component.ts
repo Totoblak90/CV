cca56a5f45760cdb62b6e0869f8d7ce671829fee
Tobias Blaksley <tobias.blaksleyiraola@cordialfinanciera.com.ar>
2021-06-23 18:30:24 -0300

    terminado sin descarga de pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { jsPDF } from "jspdf";
+import * as html2pdf from 'html2pdf.js';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,22 +10,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  
+  @ViewChild('spanishDiv', {static: false} ) spanishDiv!: ElementRef;
+  @ViewChild('englishDiv', {static: false} ) englishDiv!: ElementRef;
+
 
   title = 'CV';
-  lenguage: boolean = false;
+  language: boolean = false;
   languageMessage: string = "";
+  downloadMessage: string = "";
+
+  constructor() {
+  }
 
   ngOnInit () {
     this.languageMessage = "Switch Language"
+    this.downloadMessage = "Download as PDF"
   }
 
   switchLanguage() {
-    this.lenguage = !this.lenguage
-    if (this.lenguage) {
+    this.language = !this.language
+    if (this.language) {
       this.languageMessage = "Cambiar idioma"
+      this.downloadMessage = "Descargar como PDF"
     }
-    if (!this.lenguage) {
-      this.languageMessage = "Switch lenguage"
+    if (!this.language) {
+      this.languageMessage = "Switch language"
+      this.downloadMessage = "Download as PDF"
     }
+  }
+
+  download( language: boolean ) {
+
+    if (language === true) {
+
+
+
+      // const doc = new jsPDF();
+
+      // const specialElementHandlers = {
+      //   '#editor': function (element: any, renderer: any) {
+      //     return true;
+      //   }
+      // };
+
+      const spanishDiv = this.spanishDiv.nativeElement;
+      html2pdf(spanishDiv)
+
+      // doc.html(spanishDiv, {
+      //   callback: function (pdf) {
+      //     var iframe = document.createElement('iframe');
+      //     document.body.appendChild(iframe);
+      //     iframe.src = pdf.output('datauristring');
+      //   }
+      // });
+      // doc.save('Spanish-CV')
+
+    } else {
+
+      const doc = new jsPDF();
+
+      const specialElementHandlers = {
+        '#editor': function (element: any, renderer: any) {
+          return true;
+        }
+      };
+
+      const englishDiv = this.englishDiv.nativeElement;
+
+      doc.html(englishDiv, {
+        callback: function (pdf) {
+          var iframe = document.createElement('iframe');
+          document.body.appendChild(iframe);
+          iframe.src = pdf.output('datauristring');
+        }
+      });
+      doc.save('Spanish-CV')
+
+    }
+
   }
 }
