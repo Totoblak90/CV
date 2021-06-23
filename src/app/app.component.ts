2736e830be79c8c64f910e258c0ca2d0b12a1108
Tobias Blaksley <tobias.blaksleyiraola@cordialfinanciera.com.ar>
2021-06-23 14:45:41 -0300

    curriculum terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'CV';
+  lenguage: boolean = false;
+  languageMessage: string = "";
+
+  ngOnInit () {
+    this.languageMessage = "Switch Language"
+  }
+
+  switchLanguage() {
+    this.lenguage = !this.lenguage
+    if (this.lenguage) {
+      this.languageMessage = "Cambiar idioma"
+    }
+    if (!this.lenguage) {
+      this.languageMessage = "Switch lenguage"
+    }
+  }
 }
