3c965f24a3983d24d6a7c1f93149fe4d7f7a771b
Tobias Blaksley <tobias.blaksleyiraola@cordialfinanciera.com.ar>
2021-08-06 12:03:42 -0300

    actualización del CV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
 
 
   title = 'CV';
-  language: boolean = false;
+  language: boolean = true;
   languageMessage: string = "";
   downloadMessage: string = "";
 
